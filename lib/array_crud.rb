59b9f23633ca240fb18c3485d495e7f87de319c2
Waverley Leung <waverley.nyc@gmail.com>
2020-03-20 18:50:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = ["red", "orange", "yellow", "green"]
+  array << "blue"
 end
 
 def add_element_to_start_of_array(array, element)
