e70e00d868ac43606a18c7dc178627e628c3791c
Waverley Leung <waverley.nyc@gmail.com>
2020-03-20 19:08:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,10 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, 2)
+def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 2
+  
 end
 
 def retrieve_first_element_from_array(array)
