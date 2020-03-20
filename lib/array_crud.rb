00d864d34c5062283c430ceba2e24fa1978ad465
Waverley Leung <waverley.nyc@gmail.com>
2020-03-20 18:59:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,6 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["I", "am", "really", "learning"]
+  element = "wow"
+  array.unshift(element)
 
 end
 
