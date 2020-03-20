9bc4fe7df78bca7e15bb26dd9bd2e21ae3746260
Waverley Leung <waverley.nyc@gmail.com>
2020-03-20 19:00:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,11 +16,11 @@ def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
   element = "wow"
   array.unshift(element)
-
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
